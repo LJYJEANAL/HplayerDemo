8db3b59ef1a7f1fbe6011e51df4a1b4982c0808e
LJYJEANAL <798686050@qq.com>
2017-03-09 15:20:57 +0800

    Rejected commit: Default

--- app/src/main/java/smc/hplayerdemo/VFActivity.java
+++ app/src/main/java/smc/hplayerdemo/VFActivity.java
@@ -2,13 +2,20 @@ package smc.hplayerdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ViewFlipper;
 
 
-public class VFActivity extends BaseActivity {
+public class VFActivity extends BaseActivity implements GestureDetector.OnGestureListener{
+
+    private VelocityTracker velocityTracker;
+    private GestureDetector mGestureDetetor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,10 @@ public class VFActivity extends BaseActivity {
         mFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.push_up_in));
         // 设置滚出动画
         mFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.push_up_out));
+
+        mGestureDetetor = new GestureDetector(VFActivity.this);
+        mGestureDetetor.setIsLongpressEnabled(false);//解决长按屏幕后无法拖动现象
+
         Button toGallery = (Button) findViewById(R.id.toGallery);
         toGallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +38,56 @@ public class VFActivity extends BaseActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        Log.e("信息","event.getX()"+event.getX()+"--event.getY()"+event.getY()+"--event.getRawX()"+event.getRawX()+
+//                "--event.getRawY()"+event.getRawY());
+//        velocityTracker = VelocityTracker.obtain();
+//        velocityTracker.addMovement(event);
+//        velocityTracker.computeCurrentVelocity(1000);
+//        int xvelocityTracker=(int)velocityTracker.getXVelocity();
+//        int yvelocityTracker=(int)velocityTracker.getYVelocity();
+//        Log.e("信息",xvelocityTracker+"----"+yvelocityTracker);
+
+        return mGestureDetetor.onTouchEvent(event);
+    }
+
+    /**
+     * @param e
+     * @return
+     */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    /**
+     * @param e
+     */
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
